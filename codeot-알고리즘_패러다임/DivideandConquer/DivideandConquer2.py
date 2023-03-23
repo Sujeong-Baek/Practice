edfcb249924e5d5be6642944596bc2e59dd821b3
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-24 00:00:42 +0900

    [update]DivideandConquer2

---
+++ codeot-알고리즘_패러다임/DivideandConquer/DivideandConquer2.py
@@ -0,0 +1,14 @@
+# 합병 정렬 알고리즘 중 사용되는 merge 함수를 작성해 보세요.
+# merge 함수는 정렬된 두 리스트 list1과 list2를 받아서, 하나의 정렬된 리스트를 리턴합니다.
+
+def merge(list1, list2):
+    # 여기에 코드를 작성하세요
+    
+
+# 테스트 코드
+print(merge([1],[]))
+print(merge([],[1]))
+print(merge([2],[1]))
+print(merge([1, 2, 3, 4],[5, 6, 7, 8]))
+print(merge([5, 6, 7, 8],[1, 2, 3, 4]))
+print(merge([4, 7, 8, 9],[1, 3, 6, 10]))
