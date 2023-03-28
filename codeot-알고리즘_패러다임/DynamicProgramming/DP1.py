d508d9145168f354ea9f10f028015ad6b685f93b
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-29 00:13:42 +0900

    [update]피보나치수열 Memoization

--- codeot-알고리즘_패러다임/DynamicProgramming/DP1.py
+++ codeot-알고리즘_패러다임/DynamicProgramming/DP1.py
@@ -2,7 +2,13 @@
 # fib_memo는 꼭 memoization 방식으로 구현하셔야 합니다!
 def fib_memo(n, cache):
     # 여기에 코드를 작성하세요
-
+    if n < 3:
+        return n
+    if n in cache:
+        return cache[n]
+    
+    cache[n]=fib_memo(n-1,cache)+fib_memo(n-2,cache)
+    return cache[n]
 
 def fib(n):
     # n번째 피보나치 수를 담는 사전
