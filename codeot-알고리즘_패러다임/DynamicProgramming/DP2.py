9e56e022d320772d0336fb9e3c83ef5c3fbe0718
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-29 00:20:03 +0900

    [update]피보나치수열 Tabulation

--- codeot-알고리즘_패러다임/DynamicProgramming/DP2.py
+++ codeot-알고리즘_패러다임/DynamicProgramming/DP2.py
@@ -2,6 +2,12 @@
 # fib_tab는 꼭 tabulation 방식으로 구현하셔야 합니다!
 def fib_tab(n):
     # 여기에 코드를 작성하세요
+    fib_list=[0,1,1]
+
+    for i in range(3,n+1):
+        fib_list.append(fib_list[i-1]+fib_list[i-2])
+    return fib_list[n]
+    
 
 # 테스트 코드
 print(fib_tab(10))
