b74f5fb82024a908fc348f0600d7e58191053518
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-03-29 00:16:41 +0900

    [update]DP2

---
+++ codeot-알고리즘_패러다임/DynamicProgramming/DP2.py
@@ -0,0 +1,9 @@
+# n번째 피보나치 수를 찾아주는 함수 fib_tab을 작성해 보세요.
+# fib_tab는 꼭 tabulation 방식으로 구현하셔야 합니다!
+def fib_tab(n):
+    # 여기에 코드를 작성하세요
+
+# 테스트 코드
+print(fib_tab(10))
+print(fib_tab(56))
+print(fib_tab(132))
