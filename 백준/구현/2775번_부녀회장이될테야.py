31e80569c2fb0be8759914bc616ad2341cf7c4bd
백수정 <whitesj312@gamil.com>
2023-08-02 15:09:17 +0900

    update

---
+++ 백준/구현/2775번_부녀회장이될테야.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/2775
+def solution():
+    T = int(input())
+    test = []
+    for _ in range(T):
+        n = int(input())
+        k = int(input())
+        test.append((n,k))
+    print(test)
+    floor = []
+solution()
