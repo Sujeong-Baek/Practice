6ae0c5aca26435b77c3dee61c37d08fd50fbf658
sujeong <whitesj312@gmail.com>
2023-08-02 19:32:33 +0900

    update

--- 백준/2주차_이분탐색/3079번_입국심사.py
+++ 백준/2주차_이분탐색/3079번_입국심사.py
@@ -4,16 +4,14 @@ def solution():
     input = sys.stdin.readline
     N, M = map(int, input().split())
     times = [int(input()) for _ in range(N)]
-    left = 1
-    right = max(times)*M
-    answer = right
-    while left<=right:
-        mid = (left+right)//2
+    left = 0
+    right = min(times)*M
+    while left< right:
+        mid = (left + right)//2
         total = sum([mid//time for time in times])
         if total < M:
             left = mid + 1
         else:
-            answer = min(answer, mid)
-            right = mid - 1
-    print(answer)
+            right = mid -1
+    print(right)
 solution()
