4f7d1b9a49b5c3e6e9fdac694b2373ebc641ac8d
백수정 <whitesj312@gmail.com>
2023-09-08 18:38:01 +0900

    update

--- 백준/문자열/크면서작은수.py
+++ 백준/문자열/크면서작은수.py
@@ -2,14 +2,17 @@
 from itertools import permutations
 def solution():
     N = list(map(int, input().strip()))
-    answer = int(''.join(map(str, N)))
-    
+    target = int(''.join(map(str, N)))
+    answer = float('inf')
+
     for combi in permutations(N, len(N)):
         num = int(''.join(map(str, combi))) 
-        if num > answer:
-            answer = num
-            print(answer)
-            break
-    else:
+        
+        if num > target:
+            answer = min(answer, num)
+
+    if answer == float('inf'):
         print(0)
+    else:
+        print(answer)
 solution()
